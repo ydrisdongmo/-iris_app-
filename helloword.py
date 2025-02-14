7d40f0383f5d85cf7f6a4b107aeeb363514810c4
ydrisdongmo <ydrissous@gmail.com>
2025-02-14 23:30:15 +0100

    Update helloword.py

--- helloword.py
+++ helloword.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-st.title('PREMIERE TP DE IA ')
+st.title("SPREMIERE TP DE IA ")
 st.write("hello word!")
 print(df.describe())
 # Visualisation de la répartition des classes
-sns.countplot(x='species', data=df)
+sns.countplot(x='Species', data=df)
 plt.title('Distribution des espèces d\'iris')
 plt.show()
