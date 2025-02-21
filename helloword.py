c32de892643c3b0daf8d1260e217b50b49392c6a
ydrisdongmo <ydrissous@gmail.com>
2025-02-21 05:29:51 +0100

    Update helloword.py

--- helloword.py
+++ helloword.py
@@ -1,24 +1,28 @@
 import streamlit as st
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-st.sidebar.title("MON MENU")
-st.sidebar.button("click")
-st.sidebar.radio("choisie ton genre ",["homme","femme"])
-# Charger le jeu de données Iris
-df = sns.load_dataset('iris')
+import numpy as np
+import pandas as pd ;
+import joblib
 
-# Titre de l'application
-st.title("PREMIER TP SUR L'INTELLIGENCE ARTIFICIELLE")
-st.write("Hello world!")
 
-# Afficher les statistiques descriptives
-st.write(df.describe())
 
-# Visualisation de la répartition des classes
-fig, ax = plt.subplots(figsize=(10, 6))  # Créer une figure et un axe
-sns.countplot(x='species', data=df, ax=ax)  # Tracer sur l'axe spécifié
-ax.set_title('Distribution des espèces d\'iris')  # Définir le titre
+PetalLength =st.slider("PetalLength", 0.0, 10.0, value=2.0, step=0.1)
+SepalLength = st.slider("SepalLength", 0.0, 10.0, value=0.2, step=0.1)
+PetalWidth =st.slider("PetalWidth ", 0.0, 10.0, value=2.0, step=0.1)
+SepalWidth = st.slider("SepalWidth", 0.0, 10.0, value=0.3, step=0.1)
+    
+    
+    
+if st.button("Prediction sur la fleur", help="Cliquez pour envoyer les donnees", type="primary"): 
+    model= joblib.load("mon_model2.pkl")
+    scaler= joblib.load("mon_scaler.pkl")
+    
+    f = np.array([[SepalLength, SepalWidth, PetalLength, PetalWidth]])
+    X = pd.DataFrame(f,columns=['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'])
+    
+    X_transform = scaler.transform(X)
+    prediction = model.predict(X_transform)
+    
+    response = prediction[0]
 
-# Affichage du graphique avec Streamlit
-st.pyplot(fig)
+ 
+    st.write("fleur est: ", response)
