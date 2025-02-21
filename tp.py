2a5f7b583cafff38771cca3fa44a70f43d694110
ydrisdongmo <ydrissous@gmail.com>
2025-02-21 13:11:44 +0100

    Update tp.py

--- tp.py
+++ tp.py
@@ -1,29 +1,13 @@
 
-import streamlit as st
-import numpy as np
-import pandas as pd ;
-import joblib
-
-
-
-PetalLength =st.slider("PetalLength", 0.0, 10.0, value=2.0, step=0.1)
-SepalLength = st.slider("SepalLength", 0.0, 10.0, value=0.2, step=0.1)
-PetalWidth =st.slider("PetalWidth ", 0.0, 10.0, value=2.0, step=0.1)
-SepalWidth = st.slider("SepalWidth", 0.0, 10.0, value=0.3, step=0.1)
-    
-    
-    
-if st.button("Prediction sur la fleur", help="Cliquez pour envoyer les donnees", type="primary"): 
-    model= joblib.load("mon_model2.pkl")
-    scaler= joblib.load("mon_scaler.pkl")
-    
-    f = np.array([[SepalLength, SepalWidth, PetalLength, PetalWidth]])
-    X = pd.DataFrame(f,columns=['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'])
-    
-    X_transform = scaler.transform(X)
-    prediction = model.predict(X_transform)
-    
-    response = prediction[0]
-
- 
-    st.write("fleur est: ", response)
+# Importation des bibliothèques de base nécessaires
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+df = pd.read_csv('iris.csv', delimiter=";")
+# Statistiques descriptives pour comprendre la distribution des 
+# caractéristiques
+print(df.head())
+print(df.describe())
+#comment
+# Visualisation de la répartition des classes
+sns.countplot(x='Species', data=df)
