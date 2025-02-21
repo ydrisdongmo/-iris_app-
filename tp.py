bff3639814b705cbc36ae5ffe2c5c94aa7973867
ydrisdongmo <ydrissous@gmail.com>
2025-02-21 13:47:48 +0100

    Update tp.py

--- tp.py
+++ tp.py
@@ -1,9 +1,9 @@
-
+import streamlit as st 
 # Importation des bibliothèques de base nécessaires
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-df = pd.read_csv('MathE.csv', delimiter=";")
+df = pd.read_csv('MathE dataset(4).csv', delimiter=";" encodage"c1252")
 # Statistiques descriptives pour comprendre la distribution des 
 # caractéristiques
 print(df.head())
